16bd914ac7a830228fc772a025de180b75ff4a23
Darkdevil <90670797+DarkDevil1265@users.noreply.github.com>
2022-01-22 14:17:34 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -12,7 +12,7 @@ class script(object):
 ✯ 𝙱𝚄𝙸𝙻𝙳 𝚂𝚃𝙰𝚃𝚄𝚂: v1.0.1 [ 𝙱𝙴𝚃𝙰 ]"""
     SOURCE_TXT = """<b>NOTE:</b>
 - JACK MA is a private source project. 
-- Source - https://t.me/iwillsendknow  
+- [Source](https://t.me/iwillsendknow)  
 
 <b>DEVS:</b>
 - <a href=https://t.me/Movvie_Hall>Movie Hall</a>"""
@@ -21,7 +21,7 @@ class script(object):
 - Filter is the feature were users can set automated replies for a particular keyword and EvaMaria will respond whenever a keyword is found the message
 
 <b>NOTE:</b>
-1. JACK MA should have admin privillage.
+1. ROCK should have admin privillage.
 2. only admins can add filters in a chat.
 3. alert buttons have a limit of 64 characters.
 
@@ -32,15 +32,15 @@ class script(object):
 • /delall - <code>delete the whole filters in a chat (chat owner only)</code>"""
     BUTTON_TXT = """Help: <b>Buttons</b>
 
-- JACK MA Supports both url and alert inline buttons.
+- ROCK Supports both url and alert inline buttons.
 
 <b>NOTE:</b>
 1. Telegram will not allows you to send buttons without any content, so content is mandatory.
-2. JACK MA supports buttons with any telegram media type.
+2. ROCK supports buttons with any telegram media type.
 3. Buttons should be properly parsed as markdown format
 
 <b>URL buttons:</b>
-<code>[Button Text](buttonurl:http://t.me/JACK_MABOT)</code>
+<code>[Button Text](buttonurl:http://t.me/The_Ro_ck_Bot)</code>
 
 <b>Alert buttons:</b>
 <code>[Button Text](buttonalert:This is an alert message)</code>"""
