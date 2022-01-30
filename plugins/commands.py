421c5b4a9f3e010deb81ef4bbac77698cd859594
RJMALLU <98645849+RJMALLU@users.noreply.github.com>
2022-01-30 12:47:37 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -23,10 +23,10 @@ async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
             [
-                InlineKeyboardButton('🤖 𝐆𝐑𝐎𝐔𝐏', url='https://t.me/cine_makotta')
+                InlineKeyboardButton('𝐆𝐑𝐎𝐔𝐏', url='https://t.me/cine_makotta')
             ],
             [
-                InlineKeyboardButton('ℹ️ 𝐇𝐄𝐋𝐏', url=f"https://t.me/{temp.U_NAME}?start=help"),
+                InlineKeyboardButton('𝐇𝐄𝐋𝐏', url=f"https://t.me/{temp.U_NAME}?start=help"),
             ]
             ]
         reply_markup = InlineKeyboardMarkup(buttons)
@@ -42,13 +42,13 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ 𝐀𝐃𝐃 𝐌𝐄 𝐓𝐎 𝐀 𝐂𝐇𝐀𝐓 𝐆𝐑𝐎𝐔𝐏 ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('𝐀𝐃𝐃 𝐌𝐄 𝐓𝐎 𝐀 𝐂𝐇𝐀𝐓 𝐆𝐑𝐎𝐔𝐏', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 𝐈𝐍𝐋𝐈𝐍𝐄 𝐅𝐈𝐋𝐌𝐒', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 𝐆𝐑𝐎𝐔𝐏', url='https://t.me/cine_makotta')
+            InlineKeyboardButton('𝐈𝐍𝐋𝐈𝐍𝐄 𝐅𝐈𝐋𝐌𝐒', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('𝐆𝐑𝐎𝐔𝐏', url='https://t.me/cine_makotta')
             ],[
-            InlineKeyboardButton('ℹ️ 𝐇𝐄𝐋𝐏', callback_data='help'),
-            InlineKeyboardButton('😊 𝐀𝐁𝐎𝐔𝐓', callback_data='about')
+            InlineKeyboardButton('𝐇𝐄𝐋𝐏', callback_data='help'),
+            InlineKeyboardButton('𝐀𝐁𝐎𝐔𝐓', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -67,7 +67,7 @@ async def start(client, message):
         btn = [
             [
                 InlineKeyboardButton(
-                    "🤖 𝐉𝐎𝐈𝐍 𝐌𝐘 𝐔𝐏𝐃𝐀𝐓𝐄 𝐆𝐑𝐎𝐔𝐏", url=invite_link.invite_link
+                    "𝐉𝐎𝐈𝐍 𝐌𝐘 𝐔𝐏𝐃𝐀𝐓𝐄 𝐆𝐑𝐎𝐔𝐏", url=invite_link.invite_link
                 )
             ]
         ]
@@ -85,13 +85,13 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('➕ 𝐀𝐃𝐃 𝐌𝐄 𝐓𝐎 𝐂𝐇𝐀𝐓 𝐆𝐑𝐎𝐔𝐏 ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('𝐀𝐃𝐃 𝐌𝐄 𝐓𝐎 𝐂𝐇𝐀𝐓 𝐆𝐑𝐎𝐔𝐏 ', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('🔍 𝐈𝐍𝐋𝐈𝐍𝐄 𝐅𝐈𝐋𝐌𝐒', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🤖 𝐆𝐑𝐎𝐔𝐏', url='https://t.me/cine_makotta')
+            InlineKeyboardButton('𝐈𝐍𝐋𝐈𝐍𝐄 𝐅𝐈𝐋𝐌𝐒', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('𝐆𝐑𝐎𝐔𝐏', url='https://t.me/cine_makotta')
             ],[
-            InlineKeyboardButton('ℹ️ 𝐇𝐄𝐋𝐏', callback_data='help'),
-            InlineKeyboardButton('😊 𝐀𝐁𝐎𝐔𝐓', callback_data='about')
+            InlineKeyboardButton('𝐇𝐄𝐋𝐏', callback_data='help'),
+            InlineKeyboardButton('𝐀𝐁𝐎𝐔𝐓', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
